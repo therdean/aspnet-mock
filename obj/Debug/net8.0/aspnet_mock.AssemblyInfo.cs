ffa43ce3e2129e6f2f894ef5a5741b93f138530c
therdean <dejanristevski96@gmail.com>
2025-01-13 11:47:23 +0100

    test2

--- obj/Debug/net8.0/aspnet_mock.AssemblyInfo.cs
+++ obj/Debug/net8.0/aspnet_mock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnet_mock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bce510bc45443eb1d986c800e98c7b7fc5bf4cae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6dd7ac78d8506dc8f8f07c273a11239f852e6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnet_mock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnet_mock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
